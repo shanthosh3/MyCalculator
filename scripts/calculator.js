4270c56161bde406e8dd7c354c33f6cc17e7cc22
yshan <y.shanthosh@gmail.com>
2021-06-15 10:18:11 -0400

    commit all

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -58,13 +58,30 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        return number
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
         this.currentOperandTE.innerText = this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
             this.previousOperandTE.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTE.innerText = ''
         }
     }
 }
